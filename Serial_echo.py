fdcc331d4545751da3ac63b2c6728dd014034e07
Ed Navarre <enavarr@siue.edu>
2022-02-22 17:16:11 -0600

    Add files via upload

---
+++ Serial_echo.py
@@ -0,0 +1,47 @@
+import serial
+import time
+import tkinter as tk
+from tkinter import messagebox
+
+COMports = ['COM1','COM2','COM3','COM4']
+for device in COMports:
+    print(device)
+    try:
+        ser = serial.Serial(port=device,
+                            baudrate=57600,
+                            parity=serial.PARITY_NONE,
+                            stopbits=serial.STOPBITS_ONE,
+                            bytesize=serial.EIGHTBITS,
+                            timeout = 0,
+                            writeTimeout = 0
+                        ) #ensure non-blocking
+        break
+    except serial.SerialException:
+        messagebox.showerror("Error", "Error opening serial port.")
+
+    except:
+        messagebox.showerror("Error", "Other problem with serial port communication")
+
+connected = False
+
+time.sleep(0.1)
+ser.flushInput()
+time.sleep(1)
+
+while True:  #this could be the Serial Read 
+    if ser.inWaiting():
+        line = ser.readline()
+        data = line.decode("utf-8").split('\t') #ser.readline returns a binary, convert to string
+        print (data[0] + '\t' + data[1])
+        #output_file.write(line)
+
+        
+'''    
+while True:
+    received_data = ser.read()              #read serial port
+    sleep(0.03)
+    data_left = ser.inWaiting()             #check for remaining byte
+    received_data += ser.read(data_left)
+    print (received_data)                   #print received data
+    ser.write(received_data)                #transmit data serially 
+'''
---
+++ Serial_echo2.py
@@ -0,0 +1,35 @@
+import serial
+import time # Optional (required if using time.sleep() below)
+
+ser = serial.Serial(port='COM1', baudrate=57600)
+ser.write(bytes("?\n",'utf-8'))
+
+while (True):
+    # Check if incoming bytes are waiting to be read from the serial input 
+    # buffer.
+    # NB: for PySerial v3.0 or later, use property `in_waiting` instead of
+    # function `inWaiting()` below!
+    if (ser.inWaiting() > 0):
+        # read the bytes and convert from binary array to ASCII
+        data_str = ser.read(ser.in_waiting()).decode('ascii') 
+        # print the incoming string without putting a new-line
+        # ('\n') automatically after every print()
+        print(data_str, end='') 
+
+    # Put the rest of your code you want here
+    
+    # Optional, but recommended: sleep 10 ms (0.01 sec) once per loop to let 
+    # other threads on your PC run during this time. 
+    time.sleep(0.01) 
+
+# avoids 'readline' in favor of knowning how many bytes are readable.
+
+'''    
+while True:
+    received_data = ser.read()              #read serial port
+    sleep(0.03)
+    data_left = ser.inWaiting()             #check for remaining byte
+    received_data += ser.read(data_left)
+    print (received_data)                   #print received data
+    ser.write(received_data)                #transmit data serially 
+'''
