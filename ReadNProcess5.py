7cc72fc3a75f6a4275573b820bfe9c21b8405b3b
Ed Navarre <enavarr@siue.edu>
2022-04-12 19:46:21 -0500

    fixed file name concatenation of _sub

--- ReadNProcess5.py
+++ ReadNProcess5.py
@@ -134,7 +134,7 @@ class MainWindow(tk.Frame):
             # also save the processed files
             lineabsfile = str(self.linefile + "_abs.txt")  #consider changing to self.path_ext[1] for flexible extension use
             bkgabsfile = str(self.bkgfile + "_abs.txt")
-            lineabssubfile = str(lineabsfile + "_sub.txt")
+            lineabssubfile = str(self.linefile + "_abs_sub.txt")
             lineabsheader = "# File " + os.path.basename(self.linefile) + " converted to absorbance with Incident = " + str(np.around(line_incident,2)) + "\n# Time (s), Absorbance"
             bkgabsheader = "# File " + os.path.basename(self.bkgfile) + " converted to absorbance with Incident = " + str(np.around(bkg_incident,2)) + "\n# Time (s), Absorbance"
             np.savetxt(lineabsfile, np.transpose([self.data_time, self.line_abs]), delimiter=',', newline='\n', header=lineabsheader, comments='')
